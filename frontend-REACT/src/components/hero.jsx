523217fdf22ae7b8f21a05419a9762ddfb7a05fb
danielpsrb <danielpsrb01@gmail.com>
2024-03-29 18:27:00 +0700

    updated

--- frontend-REACT/src/components/hero.jsx
+++ frontend-REACT/src/components/hero.jsx
@@ -1,46 +1,53 @@
-import React, { useState } from 'react'
-import { useMutation } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-// const checkApiStatus = async () => {
-//     const api = await fetch("http://localhost:3003/");
-//     const response = await api.json();
-//     return response;
-// };
+function timeNow() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const time = `${hours}:${minutes}`
+    return time;
+}
 
 function Hero() {
     const [title, setTitle] = useState("")
     const [level, setLevel] = useState("")
+    const [status, setStatus] = useState("")
+    const [featureId, setFeatureId] = useState(0)
+    const [isStarted, setIsStarted] = useState(false)
 
-
-    const mutation = useMutation({
+    const addFeatures = useMutation({
         mutationFn: ({ title, level }) => {
             axios.post("http://localhost:3003/features/add", {
                 title,
                 level,
-            });
+            }).then(({ data: id }) => setFeatureId(id)).catch(() =>{"error"});
         }
-    })
+    });
 
-    // const  { isLoading, isError, isSuccess, data } = useQuery({
-    //     queryKey: ["addFeatures"],
-    //     queryFn: addFeatures({title, level}),
-    // });
-
-    // if (isError) return <div>opss sorry.. error loaded data...</div>
-    // if (isLoading) return <div>loading data...</div>
-
+    const breakFeatures = useMutation({
+        mutationFn: () => {
+            axios.put("http://localhost:3003/features/break", {
+                id: featureId.id,
+                break_time: timeNow(),
+            });
+            axios.get(`http://localhost:3003/features/status/${featureId}`)
+            .then((r) => setStatus(r.data.status))
+            .catch((e) => console.log('Error fetching datas'));
+        },
+    });
 
     return (
         <div className="flex hero items-center">
-            {mutation.isError ? (
-            <div>An error occurred: {mutation.error.message}</div>
+            {addFeatures.isError || breakFeatures.isError ? (
+            <div>An error occurred: {addFeatures.error ? addFeatures.error.message : breakFeatures.error.message}</div>
             ) : null}
             <div className="hero-content flex-col lg:flex-row">
                 <div className='flex flex-col gap-4'>
                     <h1 className="text-5xl font-bold">⌚CuyModoro</h1>
 
-                    <div>
+                    <div className='flex flex-col gap-3'>
                         <input
                             type="text"
                             placeholder="Fitur yang ingin Anda kerjakan"
@@ -54,13 +61,29 @@ function Hero() {
                             <option value={"enthusiast"}>Enthusiast</option>
                         </select>
                     </div>
-                    {mutation.isSuccess ? <div>Fitur added!</div> : null}
-                    <button className="btn btn-primary" onClick={() => {
-                        mutation.mutate({ title, level })
-                    }}>Submit
+                    {addFeatures.isSuccess ? <div>Fitur added !</div> : null}
+                    {status && <div>Status info: {status}</div>}
+                    {breakFeatures.isSuccess ? <div>Take a break for xx:xx minutes</div> : null}
+                    <button 
+                        className='btn btn-primary'
+                        onClick={() => {
+                            if (isStarted) {
+                                breakFeatures.mutate();
+                                setIsStarted(false);
+                            } else {
+                                addFeatures.mutate({ title, level, })
+                                setIsStarted(true)
+                            }
+                        }}
+                    >
+                        { isStarted ? "Pause": "Mulai Lagi"}
+                    </button>
+                    <button onClick={() => {
+                        alert(`feature ${title} selesai`)
+                    }}>
+                        Task Selesai
                     </button>
                 </div>
-
             </div>
         </div>
     )
